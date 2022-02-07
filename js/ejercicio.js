d8aea252484a683408ba494914b6f14685e3f693
CamiloRojas12 <jcalderonzambrano2@gmail.com>
2022-02-07 16:13:46 -0500

    Ej 3

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -4,3 +4,13 @@ console.log("Hola Mundo!¡")
 /* Ej-2 */
 var nombreAprendiz = "Camilo Rojas"
 console.log ("Bienvenido Señor " + nombreAprendiz)
+/* Ej-3 */
+let costoProducto = 12000;
+let impuestoProducto = 0.10;
+let domicilioProducto = 2000;
+let subTotal = costoProducto + (costoProducto * impuestoProducto);
+let costoTotal = subTotal + domicilioProducto; 
+console.log ("El valor del producto es de $ " + costoProducto); 
+console.log ("El subtotal con impuesto es de $ " + subTotal);
+console.log ("Costo por domicilio "+ domicilioProducto); 
+console.log ("El costo total sera de $ " + costoTotal);

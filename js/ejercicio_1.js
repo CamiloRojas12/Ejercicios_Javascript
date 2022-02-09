38bdaae0efd71f57bab10763c7a01da7a494227a
CamiloRojas12 <benitocamelo0002@gmail.com>
2022-02-09 15:39:08 -0500

    Ej 5

---
+++ js/ejercicio_1.js
@@ -0,0 +1,9 @@
+"Use strict"
+
+let aprendizSena = new Array ();
+aprendizSena [0] = "Camilo Rojas";
+aprendizSena [1] = "ByAlexCG";
+aprendizSena [2] = "Carolina Muñoz";
+aprendizSena [3] = "Liliana";
+for(let i=0; i<=3; i++)
+console.log (aprendizSena[i]);
